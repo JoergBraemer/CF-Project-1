7347a81423477659509bdbf56aaf1470e8b4b9be
JoergBraemer <joerg.braemer@online.de>
2016-05-15 16:25:13 +0000

    Changed root to products#index. Redirection to :back after creating new product. Navigation changed to link_to.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :products
-  resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
 
   get 'static_pages/about'
@@ -13,7 +12,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#landing_page'
+  root 'products#index'
   # root 'static_pages#index'
 
   # Example of regular route:
