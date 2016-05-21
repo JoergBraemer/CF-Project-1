d40625ae4c8c8db0c76b683231ce9028b4507b68
JoergBraemer <joerg.braemer@online.de>
2016-05-21 10:35:42 +0000

    Confirmation emails didn't get sent. Configured default url in environment/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
   # fix for missing secret token error
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
+  config.action_mailer.default_url_options = { :host => 'fictional-bikeshop.herokuapp.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
