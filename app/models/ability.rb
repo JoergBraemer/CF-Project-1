2c284149fecae83b0666c0ee60a311c6991a3a88
JoergBraemer <joerg.braemer@online.de>
2016-05-25 14:51:14 +0000

    Added admins role, enabled admins to destroy comments, restricted deleting, editing and creating products to admins only.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :create, Comment, :user_id => user.id
+      can :manage, Product
+      cannot [:create, :update, :destroy], Product
+    end
   end
 end
