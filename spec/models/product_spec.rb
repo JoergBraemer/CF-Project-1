08c2afd7053dd114424d54bce9c86abff24084a8
JoergBraemer <joerg.braemer@online.de>
2016-05-27 15:59:16 +0000

    added tests for the average_rating method and the validation if a name is present for new products

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+describe Product do
+	context "when the product has comments" do
+		before do
+  		@product = Product.create!(name: "race bike")
+  		@user = User.create!(email: "joergbraemer@onlinehome.de", password: "vcom3268")
+  		@product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+  		@product.comments.create!(rating: 3, user: @user, body: "Just another bike")
+  		@product.comments.create!(rating: 5, user: @user, body: "Greatest bike ever!")
+		end
+		it "returns the average rating of all comments" do
+			expect(@product.average_rating).to eq 3
+		end
+	end
+
+	context "when the product name is missing" do
+    before do
+      @invalid_product = Product.new(description: "This is a nameless bike.")
+    end
+
+    it "is not valid" do
+      expect(@invalid_product).not_to be_valid
+    end
+  end
+
+
+end
