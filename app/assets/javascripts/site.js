72aef71031f4e70561274c9d4b168407db2a0d96
JoergBraemer <joerg.braemer@online.de>
2016-06-05 20:37:19 +0000

    Added zoom for products on the show page using the jQuery plugin elevateZoom.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,17 @@
 $(document).on('ready page:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+	$('.img-zoom').elevateZoom({
+		zoomWindowPosition: 7,
+		zoomWindowWidth:150,
+		zoomWindowHeight:150,
+		responsive: true,
+		scrollZoom: true,
+		easing: true
+	});
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+  	readOnly: true,
+    score: function() {
+    return $(this).attr('data-score');
+    }
+  });
 });
